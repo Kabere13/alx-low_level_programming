ce6d81a2ed8365e44b52db0f8a83b839b69edf08
kabere13 <kaberekinyanjui12@gmail.com>
2023-03-28 03:27:55 -0700

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,11 +7,11 @@
  *
  * Return: result of the power
  */
-int_pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _poe_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
